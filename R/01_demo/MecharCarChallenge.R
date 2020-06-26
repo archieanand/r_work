429f304b02481b5c646c224521cea8a9132d5102
Archie Anand <archieanand@archies-mbp.lan>
2020-06-25 21:16:00 -0500

    adding

--- R/01_demo/MecharCarChallenge.R
+++ R/01_demo/MecharCarChallenge.R
@@ -3,6 +3,7 @@
 challenge_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 #perform linear regression
 lm(mpg ~ `vehicle length` + `vehicle weight` + `spoiler angle` + `ground clearance` + AWD,data=challenge_table)
+summary(lm(formula = mpg ~ `vehicle length` + `vehicle weight` + `spoiler angle` + `ground clearance` + AWD, data = challenge_table))
 #create scatterplot with relevant factors
 plt <- ggplot(challenge_table,aes(x=`spoiler angle`,y=mpg,color=AWD)) 
 #plot points & regression line
@@ -17,4 +18,4 @@ standard_deviation = sd(psi)
 summarize_suspension <- suspension_table %>% group_by(VehicleID) %>% summarize(Mean=mean(PSI),Median=median(PSI),variance=variance,sd=standard_deviation)
 
 #comparing psi to population
-t.test(log10(suspension_table$PSI),mu=mean(log10(population_table$Miles_Driven)))
+t.test(log10(suspension_table$PSI),mu=mean(log10(population_psi)))
